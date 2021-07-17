6a1fcd1f4edbf08a050047b3f8bc8bfaea58c236
gropmor <rari_atto@yahoo.co.jp>
2021-07-17 22:14:12 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,26 @@
 class PostsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
